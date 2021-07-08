b013334d0778e21c97e2a4f363a051e8bea5ef37
obingo77 <56453225+obingo77@users.noreply.github.com>
2021-07-08 01:43:52 +0000

    Create main.tf

---
+++ cluster/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.42.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+resource "aws_cloudwatch_log_group" "log_group" {
+  name = var.name
+}
